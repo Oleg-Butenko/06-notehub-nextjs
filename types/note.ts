2866c7dba866021a9655787836a4dceede821560
Oleh Butenko <butenkoleh@gmail.com>
2025-09-28 22:26:33 +0300

    fix note.ts

--- types/note.ts
+++ types/note.ts
@@ -1,14 +1,14 @@
-export type Note = {
+export interface Note {
 	id: string;
 	title: string;
 	content: string;
 	tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 	createdAt: string;
 	updatedAt: string;
-};
+}
 
-export type CreateNoteRequest = {
+export interface CreateNoteRequest {
 	title: string;
 	content: string;
-	tag: string;
-};
+	tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+}
